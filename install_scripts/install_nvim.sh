f2eb359f264077edd787144cfa8eeced5f03ed98
faltajan <faltajan@gmail.com>
2024-01-19 14:34:36 +0000

    update

--- install_scripts/install_nvim.sh
+++ install_scripts/install_nvim.sh
@@ -1,5 +1,5 @@
 # Ubuntu
-sudo apt-get install fuse libfuse2
+#sudo apt-get install fuse libfuse2
 
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage 
 chmod u+x nvim.appimage
